947355b05450cbb5f8960caebf599b1ea379af20
hal-liang-zhou <hal.liang.zhou@example.com>
2026-08-02 10:29:54 +0800

    第十二章 中断管理

--- User/Src/main.c
+++ User/Src/main.c
@@ -5,65 +5,63 @@
 #include "gpiox.h"
 #include "gpiodef.h"
 #include "stm32f10x.h"
-#include "stm32f10x_exti.h"
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_rcc.h"
+#include "stm32f10x_tim.h"
 #include "misc.h"
 
 #define TASK_STK_SIZE 128U
 
-static SemaphoreHandle_t xButtonSemaphore = NULL;
+static SemaphoreHandle_t xTickSemaphore = NULL;
 
-static void vTaskButtonHandler(void *pvParameters) {
+static void vTaskTickHandler(void *pvParameters) {
     (void)pvParameters;
     for (;;) {
-        if (xSemaphoreTake(xButtonSemaphore, portMAX_DELAY) == pdTRUE) {
+        if (xSemaphoreTake(xTickSemaphore, portMAX_DELAY) == pdTRUE) {
             DBG_LED = !DBG_LED;
-            DEBUG_INFO(0U, "Button pressed, toggle LED\n");
+            DEBUG_INFO(0U, "Timer IRQ -> task: tick\n");
         }
     }
 }
 
-void EXTI0_IRQHandler(void) {
+void TIM3_IRQHandler(void) {
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-    if (EXTI_GetITStatus(EXTI_Line0) == SET) {
-        (void)xSemaphoreGiveFromISR(xButtonSemaphore, &xHigherPriorityTaskWoken);
-        EXTI_ClearITPendingBit(EXTI_Line0);
+    if (TIM_GetITStatus(TIM3, TIM_IT_Update) == SET) {
+        (void)xSemaphoreGiveFromISR(xTickSemaphore, &xHigherPriorityTaskWoken);
+        TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
     }
     portEND_SWITCHING_ISR(xHigherPriorityTaskWoken);
 }
 
-static void vInitButton(void) {
-    GPIO_InitTypeDef GPIO_InitStructure;
-    EXTI_InitTypeDef EXTI_InitStructure;
+static void vInitTimer(void) {
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
     NVIC_InitTypeDef NVIC_InitStructure;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    TIM_TimeBaseStructure.TIM_Prescaler = 7199U;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseStructure.TIM_Period = 999U;
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
 
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOA, GPIO_PinSource0);
+    TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+    TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
 
-    EXTI_InitStructure.EXTI_Line = EXTI_Line0;
-    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
-    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-    EXTI_Init(&EXTI_InitStructure);
-
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY;
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0U;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
+
+    TIM_Cmd(TIM3, ENABLE);
 }
 
 void main(void) {
     vGpio_Init();
-    vInitButton();
-    xButtonSemaphore = xSemaphoreCreateBinary();
-    (void)xTaskCreate(vTaskButtonHandler, "Button", TASK_STK_SIZE, NULL, 1U, NULL);
+    vInitTimer();
+    xTickSemaphore = xSemaphoreCreateBinary();
+    (void)xTaskCreate(vTaskTickHandler, "Tick", TASK_STK_SIZE, NULL, 1U, NULL);
     vTaskStartScheduler();
 }
